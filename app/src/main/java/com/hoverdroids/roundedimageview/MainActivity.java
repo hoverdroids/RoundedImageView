00c206c54619d928da9b5e70e88e3a0ebde7cef8
Chris Sprague <spragucm@gmail.com>
2018-10-25 09:47:15 +0200

    Code if up to date with old repo code and maven repo connection to picasso. It loads but doesn't work - ie it doesn't show anything and it crashes upon loading because too much memory is used; this was fixed by commenting out the adapter code that loaded too many images. This needs to be revamped to work as intended.

--- app/src/main/java/com/hoverdroids/roundedimageview/MainActivity.java
+++ app/src/main/java/com/hoverdroids/roundedimageview/MainActivity.java
@@ -1,24 +1,80 @@
 package com.hoverdroids.roundedimageview;
 
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v7.widget.Toolbar;
-import android.os.Bundle;
 import android.content.Intent;
 import android.net.Uri;
-
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.Spinner;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    @Override protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //TODO apply with the toolbar since the is the main actionbar here
         applyHoverDroidsBranding();
+        getSupportActionBar().hide();
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Spinner navSpinner = (Spinner) findViewById(R.id.spinner_nav);
+
+        navSpinner.setAdapter(ArrayAdapter.createFromResource(
+                navSpinner.getContext(),
+                R.array.action_list,
+                android.R.layout.simple_spinner_dropdown_item));
+
+        navSpinner.setOnItemSelectedListener(this);
+
+        if (savedInstanceState == null) {
+            navSpinner.setSelection(0);
+        }
     }
+
+    @Override public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        Fragment newFragment;
+        switch (position) {
+            default:
+            case 0:
+                // bitmap
+                newFragment = RoundedFragment.getInstance(RoundedFragment.ExampleType.DEFAULT);
+                break;
+            case 1:
+                // oval
+                newFragment = RoundedFragment.getInstance(RoundedFragment.ExampleType.OVAL);
+                break;
+            case 2:
+                // select
+                newFragment = RoundedFragment.getInstance(RoundedFragment.ExampleType.SELECT_CORNERS);
+                break;
+            case 3:
+                // picasso
+                newFragment = new PicassoFragment();
+                break;
+            case 4:
+                // color
+                newFragment = new ColorFragment();
+                break;
+            case 5:
+                // background
+                newFragment = RoundedFragment.getInstance(RoundedFragment.ExampleType.BACKGROUND);
+                break;
+        }
+        /*
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.fragment_container, newFragment)
+                .commit();*/
+    }
+
+    @Override public void onNothingSelected(AdapterView<?> parent) { }
 
     //None of this is essential for the example. It is only used to brand HoverDroids examples
     private void applyHoverDroidsBranding() {
